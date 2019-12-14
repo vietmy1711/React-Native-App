c8cd2817958b533af8e5e0a8a398e04326b973b9
Nguyen Viet My <zzvietmyzz@gmail.com>
2019-12-14 18:22:13 +0700

    Grid Tile

--- food-app/screens/CategoriesScreen.js
+++ food-app/screens/CategoriesScreen.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button} from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
+
+    const renderGridItem = (itemData) => {
+        return (
+            <CategoryGridTile 
+            title={itemData.item.title} 
+            color={itemData.item.color}
+            onSelect={()=>{
+                props.navigation.navigate({
+                    routeName: 'CategoryMeals', 
+                    params: {
+                    categoryId: itemData.item.id
+                    }
+                })
+            }}/>
+        );
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-            <Button title="Go to meals" onPress={}/>
-        </View>
+        <FlatList 
+        keyExtractor={(item,index) => item.id}
+        data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
     );
 };
 
