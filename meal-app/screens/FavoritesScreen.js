abc1dc3f7f5d6781694bcb80dbbc65b2344bac90
Nguyen Viet My <zzvietmyzz@gmail.com>
2019-12-18 00:36:49 +0700

    Finished Meal App with Redux

--- meal-app/screens/FavoritesScreen.js
+++ meal-app/screens/FavoritesScreen.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
+import { View, Text, StyleSheet } from 'react-native';
 
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
-import { MEALS } from '../data/dummy-data';
+import DefaultText from '../components/DefaultText';
+
+
 
 const FavoritesScreen = props => {
-  const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+  const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if(favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
@@ -26,5 +38,13 @@ FavoritesScreen.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default FavoritesScreen;
